9480ea238f7e9c10dd9c0443182aa92670d41f02
maanuel <manuelmolinagom@gmail.com>
2024-11-01 15:30:38 -0600

    LineasOptimas

---
+++ src/unidad2/asignasion/LineasOptimasTest.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+package unidad2.asignasion;
+
+/** Ti301
+ *   
+ * @author Manuel Alejandro Gómez Molina
+ */
+public class LineasOptimasTest {
+    public static void main(String[] args) {
+        int[][] costos = {
+            {9, 0, 3, 0},
+            {0, 10, 4, 1},
+            {4, 5, 0, 4},
+            {0, 2, 4, 6}
+        };
+
+        LineasOptimas transporte = new LineasOptimas(costos);
+        int[][] resultado = transporte.resolver();
+
+        System.out.println("");
+        System.out.println("Salida:");
+        transporte.imprimirMatriz(resultado);
+        
+        
+        
+    }
+}
